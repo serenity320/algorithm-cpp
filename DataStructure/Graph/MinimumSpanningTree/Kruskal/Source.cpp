8e9b55ef98243ccd63263fb0ffdc969369d3ce5b
Serenity <kim910320@naver.com>
2019-06-24 01:37:29 +0900

    update

--- DataStructure/Graph/MinimumSpanningTree/Kruskal/Source.cpp
+++ DataStructure/Graph/MinimumSpanningTree/Kruskal/Source.cpp
@@ -1,7 +1,9 @@
-// https://www.geeksforgeeks.org/greedy-algorithms-set-2-kruskals-minimum-spanning-tree-mst/
-// https://www.geeksforgeeks.org/kruskals-minimum-spanning-tree-using-stl-in-c/
+#include <cstdio>
 
 int main()
 {
+    // https://www.geeksforgeeks.org/greedy-algorithms-set-2-kruskals-minimum-spanning-tree-mst/
+    // https://www.geeksforgeeks.org/kruskals-minimum-spanning-tree-using-stl-in-c/
+
     return 0;
 }
--- DataStructure/Graph/ShortestPath/BellmanFord/Source.cpp
+++ DataStructure/Graph/ShortestPath/BellmanFord/Source.cpp
@@ -1,6 +1,8 @@
-// https://www.geeksforgeeks.org/dynamic-programming-set-23-bellman-ford-algorithm/
+#include <cstdio>
 
 int main()
 {
+    // https://www.geeksforgeeks.org/dynamic-programming-set-23-bellman-ford-algorithm/
+
     return 0;
 }
